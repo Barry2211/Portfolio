0693c123a5330b2e99c1b500830c9ff071882c1f
Shakthi Ganeshan <100307146+Barry2211@users.noreply.github.com>
2025-08-11 12:40:34 +0530

    QA Resume

--- script.js
+++ script.js
@@ -30,8 +30,8 @@ document.querySelectorAll('.menu-list-items a').forEach(link => {
 //Download button functionality
 document.getElementById('getResumeBtn').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = 'Assests/Shakthi Ganeshan J Resume.pdf';
-    link.download = 'Shakthi Ganeshan J Resume.pdf';
+    link.href = 'Assests/Shakthi Ganeshan QA 2 Page Resume.pdf';
+    link.download = 'Shakthi Ganeshan QA 2 Page Resume.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
